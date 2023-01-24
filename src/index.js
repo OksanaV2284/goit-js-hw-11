a0df8c7f9db7bf126de921b86cefab9597bd3e95
OksanaV2284 <110124340+OksanaV2284@users.noreply.github.com>
2023-01-24 11:16:47 +0200

    add file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+
+import SimpleLightbox from 'simplelightbox';
+console.log(SimpleLightbox);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+console.log(Notiflix);
